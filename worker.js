cbf51d00915852ce01fb377a2838a0426a018ec1
massimoquirolo <massimoaquirolo@gmail.com>
2025-10-30 16:36:19 -0300

    FIX: Worker ya no desconecta la DB

--- worker.js
+++ worker.js
@@ -3,18 +3,13 @@
 const mongoose = require('mongoose');
 const Medicamento = require('./models/Medicamento');
 
-// Convertimos el worker en una función que podemos "llamar"
+// Esta función AHORA ASUME que Mongoose YA está conectado
+// gracias a index.js
 const ejecutarDescuentoStock = async () => {
   console.log('--- Iniciando Worker de CuidarMed (disparado por Cron) ---');
-  let conexion; // Variable para guardar la conexión
 
   try {
-    // 1. Conectarnos a la base de datos
-    // Usamos una variable para poder cerrarla después
-    conexion = await mongoose.connect(process.env.MONGO_URI);
-    console.log('Worker conectado a MongoDB.');
-
-    // 2. Obtener la hora ACTUAL en Argentina
+    // 1. Obtener la hora ACTUAL en Argentina
     const opcionesHora = {
       timeZone: 'America/Argentina/Buenos_Aires', // ¡Tu zona horaria!
       hour: '2-digit',
@@ -25,7 +20,7 @@ const ejecutarDescuentoStock = async () => {
 
     console.log(`Hora actual (Argentina): ${horaActualArgentina}`);
 
-    // 3. Buscar y actualizar medicamentos
+    // 2. Buscar y actualizar medicamentos
     const resultado = await Medicamento.updateMany(
       { 
         horarios: horaActualArgentina, 
@@ -43,16 +38,13 @@ const ejecutarDescuentoStock = async () => {
     }
 
   } catch (error) {
+    // Imprimimos el error si el worker falla
     console.error('Error en el worker:', error);
   } finally {
-    // 4. Desconectarnos de la base de datos (si logramos conectarnos)
-    if (conexion) {
-      await mongoose.disconnect();
-      console.log('Worker desconectado de MongoDB.');
-    }
-    console.log('--- Worker de CuidarMed finalizado ---');
+    // 3. Ya NO nos desconectamos
+    console.log('--- Worker de CuidarMed finalizado (dejando conexión abierta) ---');
   }
 };
 
-// ¡LA PARTE CLAVE! Exportamos la función para que index.js pueda usarla
+// Exportamos la función para que index.js pueda usarla
 module.exports = { ejecutarDescuentoStock };
